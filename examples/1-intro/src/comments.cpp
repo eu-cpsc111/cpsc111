c2f4170e204af041d387322c674c4070d3c043dd
Victor Bushong <victorbush@gmail.com>
2024-08-18 22:00:59 -0500

    Reorganize module 1 example project and add comments examples.

---
+++ examples/1-intro/src/comments.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, world!" << endl;
+    //cout << "Hello, world!" << endl;
+
+    // Single line comment
+
+    /*
+    Multi
+    line
+    comment
+    */
+
+    /* Multi line comment, but on single line */
+
+// comment
+    int a = 1; // comment
+    int /* comment */ b = 2;
+    cout << a << b << endl;
+
+    /* // valid */
+
+    // this would be invalid: /* /**/ */
+
+    return 0;
+}
